814b239f5fdab1b7af8f9b26da22e46cff515efb
Sonu Kumar Kushwaha <flyingsonu736@gmail.com>
2021-09-23 01:25:04 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,14 +2,8 @@
 
 const textarea = document.querySelector('#feed-textarea > ul');
 
-const date = new Date();
-document.querySelector('#date').innerHTML = date.toDateString();
 
 var url = 'https://www.youtube.com/feeds/videos.xml?playlist_id=PLJNqgDLpd5E69Kc664st4j7727sbzyx0X'
-
-
-
-
 
 feednami.load(url, function (result) {
     if (result.error) {
@@ -20,7 +14,6 @@ feednami.load(url, function (result) {
         for (var i = 0; i < entries.length; i++) {
             var entry = entries[i]
 
-           
 
             let li = document.createElement('li');
            
