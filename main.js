70ffd506f831e6f53976db7772ff6c69ea6cf5d3
Sonu Kumar Kushwaha <flyingsonu736@gmail.com>
2021-11-22 12:21:51 +0530

    Update main.js

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ feednami.load(url, function(result) {
             li.innerHTML = `
                 <h4>
                     <a href="${entry.link}"> 
-                        <img align="center" src="${entry.image.url}"  height="360"/>
+                        <img align="center" src="${entry.image.url}" />
                         <p>${entry.title}</p>
                     </a>
                 </h4>
